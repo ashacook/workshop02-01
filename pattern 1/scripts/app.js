89dab27dc25b92aa903582546ae5db2e1a768466
ashacook <35233826+ashacook@users.noreply.github.com>
2018-03-28 15:19:22 -0400

    Add files via upload

---
+++ pattern 1/scripts/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// col-1 blue and pink stripes
+	for (i=1;i<51;i++){
+		$('#col-1').append('<div class="stripe"></div>')
+	}
+
+	// col-3 brown and pink stripes
+	for (i=1;i<11;i++){
+		$('#col-3').append('<div class="stripe"></div>')
+	};
+});
